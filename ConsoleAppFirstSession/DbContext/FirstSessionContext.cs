1a6a03bb225074f4d36f3ae31e39576ee7651ccd
gilapex <gcarranza@apexsystems.com>
2025-02-18 12:40:26 -0700

    Initial commit

---
+++ ConsoleAppFirstSession/DbContext/FirstSessionContext.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleAppFirstSession.Models;
+
+namespace ConsoleAppFirstSession.DbContext;
+
+using Microsoft.EntityFrameworkCore;
+
+public class FirstSessionContext : DbContext
+{
+    public FirstSessionContext()
+    {
+        
+    }
+    
+    public FirstSessionContext(DbContextOptions<FirstSessionContext> options) : base(options)
+    {
+        
+    }
+    
+    public DbSet<User> Users { get; set; }
+}
---
+++ ConsoleAppFirstSession/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleAppFirstSession.DbContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile($"appsettings.json", true, true);
+
+IConfiguration config = builder.Build();
+
+var services = new ServiceCollection();
+services.AddDbContext<FirstSessionContext>(options => 
+                                    options.UseSqlServer(config.GetConnectionString("DefaultConnection")));            
+
+var serviceProvider = services.BuildServiceProvider(); 
+
+var _appDbContext = serviceProvider.GetService<FirstSessionContext>();
+foreach (var user in _appDbContext.Users.ToList())
+{
+    Console.WriteLine(user.UserName);
+    Console.WriteLine(user.Email);
+    Console.WriteLine(user.Password);
+}
